92d7f326c205c71db8dcbc6c4a3bde8c065c5754
Askibi <dinonavi82@gmail.com>
2025-02-15 14:14:38 +0300

    3глава+Упражнения

---
+++ Exercises/Exercises Chapter03/Exercise04/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число1: ");
+var число1 = Console.ReadLine()!;
+Console.WriteLine("Введите число2: ");
+var число2 = Console.ReadLine()!;
+
+ var parse1 = int.TryParse(число1, out int Z ); 
+ var parse2 =(int.TryParse(число2, out int X));
+ int c= 0 ;
+ if (parse1 & parse2 ) 
+ {
+   c = Z / X;
+ }
+ Console.WriteLine(c);
+ 
+
